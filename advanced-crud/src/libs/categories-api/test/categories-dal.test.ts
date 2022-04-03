eba784e3cdd490ba2bf8cd703fea8936f1d87c10
Gjergj Kadriu <gjergjk71@gmail.com>
2022-04-03 20:42:43 +0200

    categories-api/test

---
+++ advanced-crud/src/libs/categories-api/test/categories-dal.test.ts
@@ -0,0 +1,119 @@
+import { assert, expect } from "chai";
+
+const { 
+    parsePaginationOptions,
+    parseGeneralOptions,
+    findByPkOr404,
+    findAll,
+    createCategory,
+    updateCategory,
+    deleteCategory
+} = require("../categories-dal")
+
+describe("parsePaginationOptions function", () => {
+    it("returns pagination options", (done: jest.DoneCallback) => {
+        const io = [
+            {
+                input: [
+                    {
+                        limit: 2
+                    }
+                ],
+                output: {
+                    take: 2
+                }
+            },
+            {
+                input: [
+                    {
+                        limit: 4
+                    }
+                ],
+                output: {
+                    take: 4
+                }
+            },
+            {
+                input: [
+                    {
+                        cursor: btoa("3")
+                    }
+                ],
+                output: {
+                    where: { id: { gte: Number(atob(btoa("3"))) } },
+                    take: 3
+                }
+            },
+            {
+                input: [
+                    {
+                        cursor: btoa("14"),
+                        limit: 4
+                    }
+                ],
+                output: {
+                    where: { id: { gte: Number(atob(btoa("14"))) } },
+                    take: 4
+                }
+            },
+        ]
+        for (let item of io) {
+            const output = parsePaginationOptions(...item.input);
+            assert.deepStrictEqual(output, item.output);
+        }
+        done();
+    })
+})
+
+describe("parseGeneralOptions function", () => {
+    it("returns general options", (done: jest.DoneCallback) => {
+        const io = [
+            {
+                input: [
+                    {}
+                ],
+                output: {}
+            },
+            {
+                input: [
+                    { 
+                        expand: []
+                    }
+                ],
+                output: {
+                    include: {}
+                }
+            },
+            {
+                input: [
+                    {
+                        expand: [ "posts" ]
+                    }
+                ],
+                output: {
+                    include: {
+                        posts: true
+                    }
+                }
+            },
+            {
+                input: [
+                    {
+                        expand: [ "posts", "_count" ]
+                    }
+                ],
+                output: {
+                    include: {
+                        posts: true,
+                        _count: true
+                    }
+                }
+            },
+        ]
+        for (let item of io) {
+            const output = parseGeneralOptions(...item.input);
+            assert.deepStrictEqual(output, item.output);
+        }
+        done();
+    })
+})
